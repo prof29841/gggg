b494966bf508dd4ef564dd64530361da427723ba
prof29841 <prof29841@gmail.com>
2025-02-18 09:43:48 +0300

    first commit

---
+++ app/src/main/java/com/example/myapplication/A.java
@@ -0,0 +1,8 @@
+package com.example.myapplication;
+
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/prof29841/hg.git
+git push -u origin main
